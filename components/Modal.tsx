39221ea255a2819e627ea21469e2e3590753a958
c_y <c.eissler123@gmail.com>
2023-07-16 11:46:45 +0200

    typo

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,26 +2,26 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { IoMdClose } from 'react-icons/io'
 
 interface ModalProps {
-	isOpen: boolean
-	onChange: (open: boolean) => void
-	title: string
-	description: string
-	children: React.ReactNode
+  isOpen: boolean
+  onChange: (open: boolean) => void
+  title: string
+  description: string
+  children: React.ReactNode
 }
 
 const Modal: React.FC<ModalProps> = ({
-	isOpen,
-	onChange,
-	title,
-	description,
-	children,
+  isOpen,
+  onChange,
+  title,
+  description,
+  children,
 }) => {
-	return (
-		<Dialog.Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
-			<Dialog.Portal>
-				<Dialog.Overlay className="bg-neutral-900/90 backdrop-blur-sm fixed inset-0" />
-				<Dialog.Content
-					className="
+  return (
+    <Dialog.Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-neutral-900/90 backdrop-blur-sm fixed inset-0" />
+        <Dialog.Content
+          className="
             fixed 
             drop-shadow-md 
             border 
@@ -42,17 +42,17 @@ const Modal: React.FC<ModalProps> = ({
             p-[25px] 
             focus:outline-none
           "
-				>
-					<Dialog.Title className="text-xl text-center font-bold mb-4">
-						{title}
-					</Dialog.Title>
-					<Dialog.Description className="mb-5 text-sm leading-normal text-center">
-						{description}
-					</Dialog.Description>
-					<div>{children}</div>
-					<Dialog.Close asChild>
-						<button
-							className="
+        >
+          <Dialog.Title className="text-xl text-center font-bold mb-4">
+            {title}
+          </Dialog.Title>
+          <Dialog.Description className="mb-5 text-sm leading-normal text-center">
+            {description}
+          </Dialog.Description>
+          <div>{children}</div>
+          <Dialog.Close asChild>
+            <button
+              className="
                 text-neutral-400 
                 hover:text-white 
                 absolute 
@@ -67,15 +67,15 @@ const Modal: React.FC<ModalProps> = ({
                 rounded-full 
                 focus:outline-none
               "
-							aria-label="Close"
-						>
-							<IoMdClose />
-						</button>
-					</Dialog.Close>
-				</Dialog.Content>
-			</Dialog.Portal>
-		</Dialog.Root>
-	)
+              aria-label="Close"
+            >
+              <IoMdClose />
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
 }
 
 export default Modal
