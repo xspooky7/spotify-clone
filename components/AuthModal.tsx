311a6aa0770a9a015fbb64a08acd7ee20f5da34a
c_y <c.eissler123@gmail.com>
2023-07-16 12:17:14 +0200

    minor fixes

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,6 +1,6 @@
 import {
-	useSessionContext,
-	useSupabaseClient,
+  useSessionContext,
+  useSupabaseClient,
 } from '@supabase/auth-helpers-react'
 import Modal from './Modal'
 import { useRouter } from 'next/navigation'
@@ -12,45 +12,45 @@ import { useEffect } from 'react'
 interface AuthModalProps {}
 
 const AuthModal: React.FC<AuthModalProps> = ({}) => {
-	const router = useRouter()
-	const supabaseClient = useSupabaseClient()
-	const { session } = useSessionContext()
-	const { isOpen, onClose } = useAuthModal()
+  const router = useRouter()
+  const supabaseClient = useSupabaseClient()
+  const { session } = useSessionContext()
+  const { isOpen, onClose } = useAuthModal()
 
-	const onChange = (open: boolean) => {
-		if (!open) {
-			onClose()
-		}
-	}
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose()
+    }
+  }
 
-	useEffect(() => {
-		if (session) {
-			router.refresh()
-			onClose()
-		}
-	}, [session, router, onClose])
+  useEffect(() => {
+    if (session) {
+      router.refresh()
+      onClose()
+    }
+  }, [session, router, onClose])
 
-	return (
-		<Modal
-			title="Welcome back"
-			description="Login to your account"
-			isOpen={isOpen}
-			onChange={() => {}}
-		>
-			<Auth
-				supabaseClient={supabaseClient}
-				theme="dark"
-				providers={['github']}
-				magicLink
-				appearance={{
-					theme: ThemeSupa,
-					variables: {
-						default: { colors: { brand: '#404040', brandAccent: '#22c55e' } },
-					},
-				}}
-			/>
-		</Modal>
-	)
+  return (
+    <Modal
+      title="Welcome back"
+      description="Login to your account"
+      isOpen={isOpen}
+      onChange={onChange}
+    >
+      <Auth
+        supabaseClient={supabaseClient}
+        theme="dark"
+        providers={['github']}
+        magicLink
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: { colors: { brand: '#404040', brandAccent: '#22c55e' } },
+          },
+        }}
+      />
+    </Modal>
+  )
 }
 
 export default AuthModal
